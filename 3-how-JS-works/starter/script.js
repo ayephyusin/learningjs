49d2b1ef919d94e63083d512a2e3dffb402c4a02
isgm154 <isgm154@ISGM154>
2020-06-19 09:37:15 +0630

    initial : commit

---
+++ 3-how-JS-works/starter/script.js
@@ -0,0 +1,102 @@
+///////////////////////////////////////
+// Lecture: Hoisting
+/*function calculateAge(year){
+    console.log(2016 - year);
+}
+calculateAge(1999);
+*/
+var john = {
+    name:'John',
+    yearOfBirth: 1990,
+    calculateAge: function(){
+        console.log(this);
+        console.log(2019 - this.yearOfBirth);
+        /*function innerFunction(){
+            console.log(this);
+        }
+        innerFunction();
+       */
+    }
+}
+john.calculateAge();
+
+var mike = {
+    name:'Nike',
+    yearOfBirth: 1984
+};
+
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////
+// Lecture: Scoping
+
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+
+
+// Example to show the differece between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: The this keyword
+
+
+
+
+
+
+
+
+
