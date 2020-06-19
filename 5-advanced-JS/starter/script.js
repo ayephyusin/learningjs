0c715604feb9345c9d03772c505c8cb6fa08dbdd
isgm154 <isgm154@ISGM154>
2020-06-19 10:22:53 +0630

    feat: Creating Objects: Function Constructors

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,4 +1,4 @@
-/*var john = {
+var john = {
     name: 'John',
     yearOfBirth: 1990,
     job: 'teacher'
@@ -10,20 +10,26 @@ var Person = function(name, yearOfBirth, job) {
     this.job = job;
 }
 
-Person.prototype.calculateAge  = function() {
-    console.log(2016 - this.yearOfBirth);
-};
+Person.prototype.calculateAge = function(){
+    console.log(2016 -  this.yearOfBirth);
+}     
 
 Person.prototype.lastName = 'Smith';
 
-var john = new Person('John', 1990, 'teacher');
-var jane = new Person('Jane', 1969, 'designer');
-var mark = new Person('Mark', 1948, 'retired');
+var john = new Person('John',1990,'teacher');
+
+var jane = new Person('Jane', 1996, 'designer');
+var mark = new Person('Mark', 1948 ,'retired');
+
+
 
 john.calculateAge();
 jane.calculateAge();
 mark.calculateAge();
-*/
+
+console.log(john.lastName);
+console.log(jane.lastName);
+console.log(mark.lastName);
 
 /*
 --- Let's build a fun quiz game in the console! ---
